9b5354c8269b86be15cc57fba9ca40d657ff97e5
Csaba Gál <csaba.gal@ad.bimobject.com>
2022-10-13 15:10:19 +0200

    remove unnused parts

--- variables.tf
+++ variables.tf
@@ -8,20 +8,8 @@ variable "location_short" {
   type        = string
 }
 
-variable "client_name" {
-  description = "Client name/account used in naming"
-  type        = string
-}
 
-variable "environment" {
-  description = "Project environment"
-  type        = string
-}
 
-variable "stack" {
-  description = "Project stack name"
-  type        = string
-}
 
 variable "resource_group_name" {
   description = "Name of the resource group to use"
